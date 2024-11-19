5d367ef6ad62fe4a82f5510908ed87509f380b33
Ravi Teja <raviteja776@gmail.com>
2024-11-19 19:34:48 +0530

    Basic Actions Implement

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
+import {updateName, updateEmail,updateMobile } from '../user/userAction';
+
 
 @Component({
   selector: 'app-form',
@@ -12,12 +14,17 @@ export class FormComponent {
     
   }
   updateName(name:any) {
-    this.store.dispatch({type:'updateName', payload:{name:name.value}});
+    // this.store.dispatch({type:'updateName', payload:{name:name.value}});
+    this.store.dispatch(updateName(name.value));
   }
   updateEmail(email:any) {
-    this.store.dispatch({type:'updateEmail', payload:{email:email.value}});
+    // this.store.dispatch({type:'updateEmail', payload:{email:email.value}});
+    this.store.dispatch(updateEmail(email.value));
+
   }
   updateMobile(mobile:any) {
-    this.store.dispatch({type:'updateMobile', payload:{mobile:mobile.value}});
+    // this.store.dispatch({type:'updateMobile', payload:{mobile:mobile.value}});
+    this.store.dispatch(updateMobile(mobile.value));
+
   }
 }
---
+++ src/app/user/userAction.ts
@@ -0,0 +1,20 @@
+export function updateName(name: any) {
+    return  {
+        type:'updateName',
+        payload:{name:name}
+    }
+}
+
+export function updateEmail(name: any) {
+    return  {
+        type:'updateEmail',
+        payload:{email:name}
+    }
+}
+
+export function updateMobile(name: any) {
+    return  {
+        type:'updateMobile',
+        payload:{mobile:name}
+    }
+}
